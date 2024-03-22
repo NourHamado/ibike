60b07592053a03a75ef986fb6b77485ca7ee524f
NourHamado <nour.hamado2003@gmail.com>
2024-03-22 09:11:44 -0400

    update

--- modules/form0.py
+++ modules/form0.py
@@ -2,13 +2,13 @@ import streamlit as st
 from streamlit import session_state as ss
 
 def form_0():
+	st.button("Back", on_click=toggle_survey_state)
 	st.markdown(
 		"""
 		<p style='font-size: 20px; margin-bottom: 0;'>Do you want to take the survey? If you want to take please proceed.</p>
 		""",
 		unsafe_allow_html=True
 	)
-	st.button("Back", on_click=toggle_survey_state)
 		#ss.survey_active = False;
 
 def toggle_survey_state():
