2809133cf9da8f518c06173b006608838a45aad5
NourHamado <nour.hamado2003@gmail.com>
2024-04-16 15:47:01 -0400

    update

--- modules/instructor.py
+++ modules/instructor.py
@@ -111,7 +111,7 @@ def init():
 			name_query = st.text_input("What is your name?")
 			name_submission = st.form_submit_button("Submit")
 			if (name_submission):
-				if (name_query == "Nour" or name_query == "Faisal Aqlan" or name_query == "mur37" or name_query == "cpn10" or name_query =="mes35"):
+				if (name_query == "Nour" or name_query == "Faisal Aqlan" or name_query == "Mohammad Rasouli" or name_query == "Chetan Nikhare" or name_query =="Matthew Swinarski"):
 					ss.name = name_query
 					st.experimental_rerun() #causes the submit button to only need to be pressed once
 				else:
